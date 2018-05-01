6e8d4f28bbd7c0db090604ae99d4141aece5fc89
temhota <37623333+temhota@users.noreply.github.com>
2018-05-02 01:04:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker'
 import Posts from './Posts';
 
 ReactDOM.render(<Posts/>, document.getElementById('root'));
+registerServiceWorker()
